c54590a90d507cca26028720e35d3a1bad6ae814
Samir E. Madi <elmadisamir2002@gmail.com>
2024-11-21 16:25:50 +0000

    piff.pl
    sub d0main

--- piff.pl
+++ piff.pl
@@ -2,19 +2,7 @@
 use strict;
 use warnings;
 
-my $input_image = $ARGV[0];
-my $output_image = $ARGV[1];
-my $message = $ARGV[2];
-
-open my $in_fh, "<", $input_image or die $!;
-binmode $in_fh;
-my $image = do { local $/; <$in_fh> };
-
-my $message_bits = unpack("B*", $message);
-my $message_length = length($message_bits);
-my $message_length_bits = sprintf("%064b", $message_length);
-
-substr($image, 0, 64, $message_length_bits);
+my $input_image = $ARGV[0];ge_length_bits);
 for (my $i = 0; $i < length($message_bits); $i++) {
   substr($image, 64 + $i, 1, substr($message_bits, $i, 1));
 }
@@ -22,4 +10,4 @@ for (my $i = 0; $i < length($message_bits); $i++) {
 open my $out_fh, ">", $output_image or die $!;
 binmode $out_fh;
 print $out_fh $image;
-close $out_fh;
+close $out_fh; 
